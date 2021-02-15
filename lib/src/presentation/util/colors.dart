1385ad52368f3b90784d7cc8a8051193b3a8cb39
mohamedelsharkawy <Mohamedgamal@fci.helwan.edu.eg>
2021-02-15 17:41:56 +0200

    Add color codes + Add custom error widget

---
+++ lib/src/presentation/util/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+import 'package:flutter/material.dart';
+
+const Color PRIMARY_COLOR = Color(0xff5559d3);
+const Color ACCENT_COLOR = Color(0xfff08240);
+const Color TEXT_COLOR = Color(0xff3E4249);
+const Color GRAY_COLOR = Color(0xffC9C9C9);
+const Color GRAY_LIGHT_COLOR = Color(0xffF1F1F1);
