ba28aebeaadffcd31425defe8fca2908b9403ae6
mohamedelsharkawy <Mohamedgamal@fci.helwan.edu.eg>
2021-02-22 16:58:32 +0200

    Add Login & SignUp buttons to Choose Authentication Type UI

--- lib/src/presentation/ui/choose_auth.dart
+++ lib/src/presentation/ui/choose_auth.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:letstravel/src/presentation/util/colors.dart';
+import 'package:letstravel/src/presentation/util/constants.dart';
 import 'package:letstravel/src/presentation/util/images_path.dart';
+
+import 'home.dart';
 
 class ChooseAuthType extends StatelessWidget {
   @override
@@ -15,28 +18,88 @@ class ChooseAuthType extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           ),
-          child: Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  margin: EdgeInsets.only(top: 150),
-                  padding: EdgeInsets.symmetric(horizontal: 32, vertical: 56),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(16),
-                    color: KPRIMARY_COLOR,
-                  ),
-                  child: Text(
-                    "Let's Travel",
-                    style: TextStyle(
-                      fontSize: 24,
-                      fontFamily: 'CairoBold',
-                      color: Colors.white,
-                    ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Container(
+                margin: EdgeInsets.only(top: 150),
+                padding: EdgeInsets.symmetric(horizontal: 32, vertical: 56),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(16),
+                  color: KPRIMARY_COLOR,
+                ),
+                child: Text(
+                  "Let's Travel",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontFamily: 'CairoBold',
+                    color: Colors.white,
                   ),
                 ),
-              ],
-            ),
+              ),
+              SizedBox(height: 80),
+              Expanded(
+                  child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    width: double.infinity,
+                    height: 56,
+                    margin: EdgeInsets.all(32),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => Home(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Login",
+                        style: TextStyle(
+                          fontFamily: CAIRO_SEMI_BOLD,
+                          fontSize: 20
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: ACCENT_COLOR,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          )),
+                    ),
+                  ),
+                  Container(
+                    width: double.infinity,
+                    height: 56,
+                    margin: EdgeInsets.only(left: 32,right: 32,top: 8),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                          context,
+                          MaterialPageRoute(
+                            builder: (_) => Home(),
+                          ),
+                        );
+                      },
+                      child: Text(
+                        "Create Account",
+                        style: TextStyle(
+                            fontFamily: CAIRO_SEMI_BOLD,
+                            fontSize: 20
+                        ),
+                      ),
+                      style: ElevatedButton.styleFrom(
+                          primary: ACCENT_COLOR,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(16),
+                          )),
+                    ),
+                  )
+
+                ],
+              ))
+            ],
           ),
         ),
       ),
---
+++ lib/src/presentation/util/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const String CAIRO_REG='CairoRegular';
+const String CAIRO_SEMI_BOLD='CairoSemiBold';
+const String CAIRO_BOLD='CairoBold';
